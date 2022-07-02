3c3dbcd44e0d1974c17fb7b0eb713ce7daf17cd7
enijkamp <erik.nijkamp@gmail.com>
2022-07-01 20:49:27 -0700

    fix for sanity checks #2

--- progen2/likelihood.py
+++ progen2/likelihood.py
@@ -82,31 +82,33 @@ def cross_entropy(logits, target, ignore_index, reduction='mean'):
     return torch.nn.functional.cross_entropy(input=logits, target=target, weight=None, size_average=None, ignore_index=ignore_index, reduce=None, reduction=reduction)
 
 
-def cross_entropy_with_shift(logits, target, ignore_index, reduction):
-    shift_logits = logits[..., :-1, :].contiguous()
-    shift_target = target[..., 1:].contiguous()
-    return cross_entropy(shift_logits.view(-1, shift_logits.size(-1)), shift_target.view(-1), ignore_index=ignore_index, reduction=reduction)
-
-
 def log_likelihood(logits, target, ignore_index, reduction='mean'):
     with torch.no_grad():
-        return -cross_entropy_with_shift(logits=logits, target=target, ignore_index=ignore_index, reduction=reduction)
+        shift_logits = logits[..., :-1, :].contiguous()
+        shift_target = target[..., 1:].contiguous()
+        return -cross_entropy(shift_logits.view(-1, shift_logits.size(-1)), shift_target.view(-1), ignore_index=ignore_index, reduction=reduction)
 
 
 def log_likelihood_custom_1(logits, target, ignore_index, reduction='mean'):
     with torch.no_grad():
-        return -torch.nn.functional.nll_loss(input=torch.log_softmax(logits, dim=1), target=target, reduction=reduction, ignore_index=ignore_index)
+        shift_logits = logits[..., :-1, :].contiguous()
+        shift_target = target[..., 1:].contiguous()
+        return -torch.nn.functional.nll_loss(input=torch.log_softmax(shift_logits, dim=1), target=shift_target, reduction=reduction, ignore_index=ignore_index)
 
 
 def log_likelihood_custom_2(logits, target, ignore_index, reduction='mean'):
     with torch.no_grad():
         assert len(target.shape) == 1
         assert logits.shape[0] == target.shape[0]
+
+        shift_logits = logits[..., :-1, :].contiguous()
+        shift_target = target[..., 1:].contiguous()
+
         log_likelihood = 0.0
-        n = (target != ignore_index).long().sum()
-        for i in range(logits.shape[0]):
-            if target[i] != ignore_index:
-                log_likelihood += torch.log_softmax(logits, dim=1)[i, target[i]] / (1. if reduction == 'sum' else n)
+        n = (shift_target != ignore_index).long().sum()
+        for i in range(shift_logits.shape[0]):
+            if shift_target[i] != ignore_index:
+                log_likelihood += torch.log_softmax(shift_logits, dim=1)[i, shift_target[i]] / (1. if reduction == 'sum' else n)
         return log_likelihood
 
 
@@ -134,7 +136,8 @@ def main():
     parser.add_argument('--rng-deterministic', default=True, type=lambda x: (str(x).lower() == 'true'))
     parser.add_argument('--fp16', default=True, type=lambda x: (str(x).lower() == 'true'))
     parser.add_argument('--context', type=str, default='1MGHGVSRPPVVTLRPAVLDDCPVLWRWRNDPETRQASVDEREIPVDTHTRWFEETLKRFDRKLFIVSADGVDAGMVRLDIQDRDAAVSVNIAPEWRGRGVGPRALGCLSREAFGPLALLRMSAVVKRENAASRIAFERAGFTVVDTGGPLLHSSKARLHVVAAIQARMGSTRLPGKVLVSIAGRPTIQRIAERLAVCQELDAVAVSTSVENRDDAIADLAAHLGLVCVRGSETDLIERLGRTAARTGADALVRITADCPLVDPALVDRVVGVWRRSAGRLEYVSNVFPPTFPDGLDVEVLSRTVLERLDREVSDPFFRESLTAYVREHPAAFEIANVEHPEDLSRLRWTMDYPEDLAFVEAVYRRLGNQGEIFGMDDLLRLLEWSPELRDLNRCREDVTVERGIRGTGYHAALRARGQAP2')
-    parser.add_argument('--sanity', default=False, type=lambda x: (str(x).lower() == 'true'))
+    parser.add_argument('--sanity', default=True, type=lambda x: (str(x).lower() == 'true'))
+    parser.add_argument('--non-AA', default=False, type=lambda x: (str(x).lower() == 'true'))
     args = parser.parse_args()
 
 
@@ -163,12 +166,19 @@ def main():
 
     # (4) log likelihood
 
-    def ll(tokens, f=log_likelihood, ignore_index=pad_token_id, reduction='mean'):
+    def ll(tokens, f=log_likelihood, ignore_index=pad_token_id, reduction='mean', non_AA=args.non_AA):
         with torch.cuda.amp.autocast():
             input_ids = torch.tensor(tokenizer.encode(tokens).ids).to(device)
             logits = model(input_ids, labels=input_ids).logits
-            return f(logits=logits, target=input_ids, ignore_index=ignore_index, reduction=reduction)
 
+            if non_AA:
+                logits = logits[:,5:-2]
+                targets = input_ids[:]-5
+            else:
+                logits = logits
+                targets = input_ids
+
+            return f(logits=logits, target=targets, ignore_index=ignore_index, reduction=reduction).item()
 
 
 
@@ -197,26 +207,26 @@ def main():
         with print_time('sanity model'):
 
             alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-            observation_data = '2PAQGRARLAAHYGTGRIGREVTVDERCRNLDRLEPSWELLRLLDDMGFIEGQNGLRRYVAEVFALDEPYDMTWRLRSLDEPHEVNAIEFAAPHERVYATLSERFFPDSVERDLRELVTRSLVEVDLGDPFTPPFVNSVYELRGASRRWVGVVRDVLAPDVLPCDATIRVLADAGTRAATRGLREILDTESGRVCVLGLHAALDAIADDRNEVSTSVAVADLEQCVALREAIRQITPRGAISVLVKGPLRTSGMRAQIAAVVHLRAKSSHLLPGGTDVVTFGAREFAIRSAANERKVVASMRLLALPGFAERSLCGLARPGVGRGRWEPAINVSVAADRDQIDLRVMGADVGDASVIFLKRDFRKLTEEFWRTHTDVPIEREDVSAQRTEPDNRWRWLVPCDDLVAPRLTVVPPRSVGHGM1'
-            observation_random = '2' + ''.join([random.choice(alphabet) for _ in range(len(observation_data)-2)]) + '1'
-            observation_perturb = observation_random[:64] + observation_data[len(observation_random[:64]):]
+            x_data = '2PAQGRARLAAHYGTGRIGREVTVDERCRNLDRLEPSWELLRLLDDMGFIEGQNGLRRYVAEVFALDEPYDMTWRLRSLDEPHEVNAIEFAAPHERVYATLSERFFPDSVERDLRELVTRSLVEVDLGDPFTPPFVNSVYELRGASRRWVGVVRDVLAPDVLPCDATIRVLADAGTRAATRGLREILDTESGRVCVLGLHAALDAIADDRNEVSTSVAVADLEQCVALREAIRQITPRGAISVLVKGPLRTSGMRAQIAAVVHLRAKSSHLLPGGTDVVTFGAREFAIRSAANERKVVASMRLLALPGFAERSLCGLARPGVGRGRWEPAINVSVAADRDQIDLRVMGADVGDASVIFLKRDFRKLTEEFWRTHTDVPIEREDVSAQRTEPDNRWRWLVPCDDLVAPRLTVVPPRSVGHGM1'
+            x_random = '2' + ''.join([random.choice(alphabet) for _ in range(len(x_data)-2)]) + '1'
+            x_perturb = x_random[:64] + x_data[len(x_random[:64]):]
 
-            print(observation_data)
-            print(observation_perturb)
-            print(observation_random)
+            print(x_data)
+            print(x_perturb)
+            print(x_random)
 
-            assert observation_data != observation_perturb
+            assert x_data != x_perturb
 
-            ll_observation_data = ll(observation_data)
-            ll_observation_random = ll(observation_random)
-            ll_observation_perturb = ll(observation_perturb)
+            ll_x_data = ll(x_data)
+            ll_x_random = ll(x_random)
+            ll_x_perturb = ll(x_perturb)
 
-            print(f'll_observation_data={ll_observation_data}')
-            print(f'll_observation_random={ll_observation_random}')
-            print(f'll_observation_perturb={ll_observation_perturb}')
+            print(f'll_x_data={ll_x_data}')
+            print(f'll_x_random={ll_x_random}')
+            print(f'll_x_perturb={ll_x_perturb}')
 
-            assert ll_observation_data > ll_observation_random
-            assert ll_observation_data > ll_observation_perturb
+            assert ll_x_data > ll_x_random
+            assert ll_x_data > ll_x_perturb
 
 
     # (7) likelihood
@@ -247,6 +257,7 @@ def main():
 
         print(f'll_sum={(ll_sum)}')
         print(f'll_mean={ll_mean}')
+
 
 
 if __name__ == '__main__':
